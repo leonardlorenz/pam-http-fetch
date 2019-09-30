fbed335e2cb42e2d2a2fc9774ca21107616cd5f6
Leonard Lorenz <bi0m3tr1xx@gmail.com>
2019-09-30 13:53:38 +0200

    added all necessary environment variables, forming a valid http request to the api, saving the list of users to the specified path

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 import requests
 import json
+import sys
+import os
 
-DOMAIN='localhost'
-PORT='8000'
-URI='users/'
+DOMAIN = os.environ['PAM_HTTP_PROVIDER_DOMAIN']
+PORT = os.environ['PAM_HTTP_PROVIDER_PORT']
+URI = os.environ['PAM_HTTP_PROVIDER_URI']
+OUTPUT_FILE_PATH = os.environ['PAM_HTTP_OUTPUT_FILE_PATH']
+TOKEN = os.environ['PAM_HTTP_AUTHENTICATION_TOKEN']
 
-users = requests.get('http://' + DOMAIN + ':' + PORT + '/' + URI)
+users = requests.get(
+        'http://' + DOMAIN + ':' + PORT + '/' + URI,
+        headers={'Authorization' : TOKEN})
+user_list = users.json()
 
-print(users.json())
+print(user_list)
+
+with open(OUTPUT_FILE_PATH, 'w') as f:
+    for user in user_list:
+        f.write(str(user['username']) + '\n')
